eeb5e314afb7b5a329bf990af71a55934549d5b2
Nephr.babiyakharka <nephrmanagement@gmail.com>
2026-02-13 11:07:51 +0545

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,11 @@
-self.addEventListener('fetch', function(event) {});
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => {
+      return new Response('Offline');
+    })
+  );
+});
